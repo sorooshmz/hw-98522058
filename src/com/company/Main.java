6e03932803dc61bc7fa511e463cdfd5215bc54c9
sorooshmz <soroosh2.1380@gmail.com>
2022-11-16 11:48:19 +0330

    letting the use chose the option

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,32 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
+
+    // this program takes a number of values and shows the average and the top value
+    // according to the chosen option
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        int num; // for holding the number of items
+        int chosen; // for saving the entered option by the use
+
+        // taking input from the use until enters the right input
+        while(true) {
+            System.out.println("enter 1 or 2 : "); // showing the list of options
+            System.out.println("1) average");
+            System.out.println("2) top value");
+            chosen = Integer.parseInt(scanner.nextLine());
+            if (chosen != 1 && chosen != 2) {
+                System.out.println("Wront input!");
+            }
+            else {
+                break;
+            }
+        }
+
+
+        
     }
 }
